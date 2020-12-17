90fd8b7745c254da89a1da3e5dd9af35dda6a5b1
sudikshaPaymode <sudikshapaymode20@gmail.com>
2020-12-17 00:42:43 -0500

    add basic project structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { ApiService } from './services/api-service.service';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionnaireComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule
+  ],
+  providers: [
+    ApiService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
